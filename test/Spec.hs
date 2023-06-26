3fb3783592dc18ddca6d568a9948b3b26c54ab32
SeungukLee <seunguk@seunguk.dev>
2023-06-26 16:32:11 +0900

    (wip) add golden test (#9)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,35 @@
+import qualified Data.ByteString.Lazy.Char8 as BS
+import           Data.Functor               ((<&>))
+import qualified Data.Text                  as T
+
+import           System.FilePath            (replaceExtension, takeBaseName)
+
+import           Test.Tasty                 (TestTree, defaultMain, testGroup)
+import           Test.Tasty.Golden          (findByExtension, goldenVsString)
+
+import           Eval                       (runEval)
+import           Parser                     (parseSL)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  paths <- listTestFiles
+  goldens <- mapM mkGoldenTest paths
+  defaultMain (testGroup "Tests" goldens)
+
+listTestFiles :: IO [FilePath]
+listTestFiles = do
+  findByExtension [".sl"] "test/Goldens"
+
+mkGoldenTest :: FilePath -> IO TestTree
+mkGoldenTest path = do
+  let testName = takeBaseName path
+  let goldenPath = replaceExtension path ".golden"
+  return (goldenVsString testName goldenPath $ action <&> BS.pack . show)
+  where
+    action = do
+      script <- readFile path
+      let p = parseSL $ T.pack script
+      case p of
+        -- TODO
+        Left _  -> undefined
+        Right e -> runEval e
