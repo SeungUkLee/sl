071c7be109ef0236c7918ba0221088b469e19d0b
SeungukLee <seunguk@seunguk.dev>
2023-08-19 18:59:36 +0900

    add `ParseError` type alias

--- src/SLang/Parser/Common.hs
+++ src/SLang/Parser/Common.hs
@@ -1,9 +1,12 @@
 module SLang.Parser.Common
   ( Parser
+  , ParseError
   ) where
 
-import           Data.Text       (Text)
-import           Data.Void       (Void)
-import           Text.Megaparsec (Parsec)
+import           Data.Text             (Text)
+import           Data.Void             (Void)
+import           Text.Megaparsec       (Parsec)
+import           Text.Megaparsec.Error (ParseErrorBundle)
 
 type Parser = Parsec Void Text
+type ParseError =  ParseErrorBundle Text Void
