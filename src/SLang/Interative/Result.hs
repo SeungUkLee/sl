9294f39f2f8ea09d84dab8746653cf70085e9e5d
SeungukLee <seunguk@seunguk.dev>
2023-10-15 21:40:01 +0900

    chore: remove unused data constructor

--- src/SLang/Interative/Result.hs
+++ src/SLang/Interative/Result.hs
@@ -6,7 +6,6 @@ module SLang.Interative.Result
 
 import           Prettyprinter        ((<+>))
 
-import qualified Data.Text            as T
 import           SLang.Eval.Domain    (Value)
 import           SLang.Eval.Syntax    (Expr)
 import qualified SLang.Pretty         as SP
@@ -17,12 +16,10 @@ data Result
   = Interpret Type Value
   | Parse Expr
   | TypeInfer Expr Type
-  | Define T.Text
   | Load Type Value
 
 instance Pretty Result where
   pretty (Interpret typ val)  = "-" <+> ":" <+> SP.pretty typ <+> "=" <+> SP.pretty val <> "\n"
   pretty (Parse ast)          = SP.pretty $ show ast <> "\n"
   pretty (TypeInfer expr typ) = SP.pretty expr <+> ":" <+> SP.pretty typ <> "\n"
-  pretty (Define txt)         = SP.pretty txt
   pretty (Load typ val)       = "-" <+> ":" <+> SP.pretty typ <+> "=" <+> SP.pretty val <> "\n"
